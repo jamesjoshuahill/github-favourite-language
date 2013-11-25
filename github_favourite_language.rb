f8fe112d4e5b8465f1ff307b6b04913868a8abd0
Josh Hill <jamesjoshuahill@gmail.com>
2013-11-25 15:23:36 +0000

    Add spec to get list of repositories

--- github_favourite_language.rb
+++ github_favourite_language.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/env ruby
 require 'thor'
+require 'octokit'
 
 class GitHubFavouriteLanguage < Thor
   package_name "GitHub Favourite Language"
+
+  def repositories(username)
+    Octokit.repositories(username)
+  end
 
 end
 
--- spec/github_favourite_language_spec.rb
+++ spec/github_favourite_language_spec.rb
@@ -2,4 +2,19 @@ require './github_favourite_language'
 
 describe GitHubFavouriteLanguage do
 
+  let(:username) { 'jamesjoshuahill' }
+
+  def repository(language)
+    double :repository, :language => language
+  end
+
+  context 'when given a valid GitHub username' do
+
+    it 'can get their repositories' do
+      expect(Octokit).to receive(:repositories).with(username).and_return :repo
+      expect(subject.repositories(username)).to eq :repo
+    end
+
+  end
+
 end
