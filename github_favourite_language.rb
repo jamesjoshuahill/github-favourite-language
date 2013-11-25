df2f18fa838efe32ca45b57862c9137bff017671
Josh Hill <jamesjoshuahill@gmail.com>
2013-11-25 17:12:25 +0000

    Fix favourite language arithmetic and map method to alias

--- github_favourite_language.rb
+++ github_favourite_language.rb
@@ -4,15 +4,24 @@ require 'octokit'
 
 class GitHubFavouriteLanguage < Thor
   package_name "GitHub Favourite Language"
+  map "-u" => "favourite_language"
 
-  def repositories(username)
-    Octokit.repositories(username)
-  end
-
+  desc "-u USERNAME", "Identify the favourite language of a GitHub user"
   def favourite_language(username)
     languages = repositories(username).group_by { |repo| repo.language }
-    languages.each { |language, repositories| repositories.map!(&:size) }
-    languages.max_by { |language, size| size }.first
+    languages.each do |language, repositories|
+      languages[language] = repositories.map(&:size).inject(:+)
+    end
+    favourite = languages.max_by { |language, size| size }.first
+    # puts "#{username}'s favourite language is #{favourite}."
+  end
+
+  no_commands do
+
+    def repositories(username)
+      Octokit.repositories(username)
+    end
+
   end
 
 end
